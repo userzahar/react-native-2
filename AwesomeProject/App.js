3d8fcfc669448e651088c222480b1dab0957b06f
userzahar <110730909+userzahar@users.noreply.github.com>
2023-05-02 12:14:35 +0300

    initialing

---
+++ AwesomeProject/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Тут могла бути ваша реклама +380 98 975 2841</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
