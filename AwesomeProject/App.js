05b8a036ead3c78c869970eb688b0e32eebb0791
userzahar <110730909+userzahar@users.noreply.github.com>
2023-06-28 16:52:50 +0300

    add bottom routes

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,39 +4,25 @@ import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {
-  StyleSheet,
-  View,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
 
 
 import CommentsScreen from "./src/Screens/CommentsScreen/CommentsScreen";
-// import CreatePostsScreen from "./src/Screens/CreatePostsScreen/CreatePostsScreen";
+import CreatePostsScreen from "./src/Screens/CreatePostsScreen/CreatePostsScreen";
 import RegistrationScreen from "./src/Screens/RegistrationScreen/RegistrationScreen";
 import LoginScreen from "./src/Screens/LoginScreen/LoginScreen";
 import PostsScreen from "./src/Screens/PostsScreen/PostsScreen";
-// import ProfileScreen  from "./src/Screens/ProfileScreen/ProfileScreen";
+import ProfileScreen  from "./src/Screens/ProfileScreen/ProfileScreen";
 
 
 
 const MainStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto: require("./src/assets/fonts/Roboto-Black.ttf"),
-  });
-  if (!fontsLoaded) {
-    return null;
-  }
 
-  return (
-    <NavigationContainer>
-        <MainStack.Navigator 
-        // initialRouteName="Home"
-        >
+const useRoute = (isAuth)=>{
+  if(!isAuth){
+      return (
+        <MainStack.Navigator>
           <MainStack.Screen 
           options={{headerShown:false}} 
             name="Registration"
@@ -47,15 +33,36 @@ export default function App() {
           <MainStack.Screen  
           options={{headerShown:false}} name="Home" component={PostsScreen} />
         </MainStack.Navigator>
+      )
+  }
+  return (
+    <MainTab.Navigator>
+      <MainTab.Screen options={{headerShown:false}} name="Home" component={PostsScreen}/>
+      <MainTab.Screen options={{headerShown:false}} name="CreatePostsScreen" component={CreatePostsScreen}/>
+      <MainTab.Screen options={{headerShown:false}} name="ProfileScreen" component={ProfileScreen}/>
+    </MainTab.Navigator>
+  )
+}
+
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto: require("./src/assets/fonts/Roboto-Black.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  const routes = useRoute(null)
+
+  return (
+    <NavigationContainer>
+          {routes}
           <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
+
+
