9158720eedb099b00ea10e27a0e78dae236f70c4
userzahar <110730909+userzahar@users.noreply.github.com>
2023-07-14 15:25:59 +0300

    add redux

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,8 +4,8 @@ import { useFonts } from "expo-font";
 // import { PersistGate } from "redux-persist/integration/react";
 import { NavigationContainer } from "@react-navigation/native";
 import useRoute from "./router";
-// import {Provider} from "react-redux"
-// import {store, persistor} from "./src/redux/store";
+import {Provider} from "react-redux"
+import {store} from "./src/redux/store";
 
 
 export default function App() {
@@ -16,13 +16,15 @@ export default function App() {
     return null;
   }
   
-  const routes = useRoute({})
+  const routes = useRoute(0===1)
   
   return (
+      <Provider store={store}>
            <NavigationContainer backBehavior="history">
                     {routes}
                     <StatusBar style="auto" />
               </NavigationContainer>
+      </Provider>
 
 
       );
